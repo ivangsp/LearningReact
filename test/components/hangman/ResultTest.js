1662c3c4e7327a8d7af37ba70e67f39941289957
ivangsp <talk2gspivan@outlook.com>
2018-04-13 09:54:03 +0300

    added tests for result compnent

--- test/components/hangman/ResultTest.js
+++ test/components/hangman/ResultTest.js
@@ -1,1 +1,41 @@
+// /* global sinon */
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import Result from '../../../src/components/hangman/Result';
+
+describe('Result', () => {
+  it('always renders a single div', () => {
+    const wrapper = mount(<Result predChar={['a', 'b']} />);
+    const divs = wrapper.find('<div>');
+    expect(divs.length).toBeGreaterThan(0);
+  });
+
+  it('div contains everything else that gets rendered', () => {
+    const wrapper = mount(<Result predChar={['a', 'b']} />);
+    const divs = wrapper.find('<div>');
+    const wrappingDiv = divs.first();
+    expect(wrappingDiv.children()).toEqual(wrapper.children());
+  });
+
+  it('always renders single image', () => {
+    const wrapper = mount(<Result predChar={['a', 'b']} />);
+    const img = wrapper.find('<img>');
+    expect(img.length).toBe(1);
+  });
+
+  it('renders', () => {
+    expect(shallow(
+      <Result predChar={[]} game={{}} >text</Result>
+    )).to.exist;
+  });
+
+
+  // it('allows us to set props', () => {
+  //   const wrapper = mount(<Result predChar={['a', 'b']} />);
+  //   expect(wrapper.props().predChar).to.equal(['a', 'b']);
+  //   wrapper.setProps({predChar: ['a', 'b', 'c']});
+  //   expect(wrapper.props().predChar).to.equal(['a', 'b', 'c']);
+  // });
+});
 
--- test/components/hangman/UserGuessTest.js
+++ test/components/hangman/UserGuessTest.js
@@ -22,32 +22,5 @@ describe('UserGuess', () => {
     expect(form).to.have.exactly(1).descendants('input');
     expect(form.find('input').length).to.eq(1);
   });
-
-//   it('calls submitGuess with user input  when onKeyUp is called', () => {
-//         const submitGuess = sinon.spy();
-//         const wrapper = mount(<UserGuess gameState='finished' submitGuess={sinon.stub()} text='text'
-//          onKeyUp= {submitGuess} />);
-//         wrapper.find('input').simulate('click');
-//         // expect(wrapper.state('change')).to.eq('');
-//         // expect(wrapper.state('keyDown')).to.equal('');
-//         expect(submitGuess.called).to.be.true;
-//     });
 });
-
-
-// describe('CommentForm', () => {
-//   // Testing callbacks and the arguments that callbacks receive is generally
-//   // very useful.
-
-
-//   // Testing any other behaviour that a component has is also useful
-//   it('clears state when submit button clicked', () => {
-//     const form = shallow(<CommentForm onSubmit={sinon.stub()} text='text' />);
-
-//     form.setState({author: 'author', text: 'text'});
-//     form.find('button').simulate('click');
-//     expect(form.state()).to.eql({author: '', text: ''});
-//   });
-// });
-
 
